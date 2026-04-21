23082bd12675230d7eb36ea3dab785372fa1546a
ezemallima <ezetombacapo@gmail.com>
2026-04-21 14:04:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -493,4 +493,4 @@ def test_modelos():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, threaded=True)
